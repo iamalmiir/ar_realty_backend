5e6ddb7b5760bfb8fac8b04b480a29f5a9b576f5
iamalmiir <iamalmiir@gmail.com>
2022-05-27 21:20:10 -0400

    first commit

--- realtors/models.py
+++ realtors/models.py
@@ -1,1 +1,15 @@
+from distutils.command.upload import upload
 from django.db import models
+
+
+class Realtor(models.Model):
+    full_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="realtors/%Y/%m/%d/")
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+    mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
