aa5ed16c44295baed6d5df6c00226c600e2175d1
iamalmiir <iamalmiir@gmail.com>
2022-05-27 21:22:37 -0400

    Added Realtor model

--- ar_realty/settings.py
+++ ar_realty/settings.py
@@ -20,14 +20,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party apps
     "rest_framwork",
-    "realtors",
+    # local apps
+    "realtors.apps.RealtorsConfig",
 ]
 
 MIDDLEWARE = [
