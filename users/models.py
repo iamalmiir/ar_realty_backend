0ddcf3f7543e602045a052171e454554784da84f
iamalmiir <iamalmiir@gmail.com>
2022-06-13 20:14:43 -0400

    improvements

--- users/models.py
+++ users/models.py
@@ -63,8 +63,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomAccountManager()
 
-    USERNAME_FIELD = "user_name"
-    REQUIRED_FIELDS = ["email", "full_name"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["user_name", "full_name"]
 
     def __str__(self):
         return self.user_name
