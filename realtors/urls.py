c130e5f2fb81eab666b3ab16219f435c28fbd57c
iamalmiir <iamalmiir@gmail.com>
2022-06-26 20:48:33 -0400

    small changes

--- realtors/urls.py
+++ realtors/urls.py
@@ -1,6 +1,7 @@
-from realtors import views
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+
+from realtors import views
 
 urlpatterns = [
     path("realtors/", views.RealtorList.as_view()),
--- realtors/views.py
+++ realtors/views.py
@@ -14,6 +14,10 @@ class RealtorList(generics.ListCreateAPIView):
 # Get the Realtor object by ID
 class RealtorDetail(generics.RetrieveAPIView):
     lookup_field = "slug"
-    queryset = Realtor.objects.all()
     serializer_class = RealtorSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        # if not found DRF will raise a 404 error
+        return Realtor.objects.filter(slug=slug)
